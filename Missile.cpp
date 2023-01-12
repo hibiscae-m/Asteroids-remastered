503aa94360fc9bc3fbd7af4646b2d6308cb7dd3f
Marius <mcouvratd@hibiscae.fr>
2023-01-12 20:25:41 +0100

    - Drastically reducing hitbox for better collision with asteroids

--- Missile.cpp
+++ Missile.cpp
@@ -3,13 +3,16 @@
 //
 
 #include "Missile.h"
+#include <limits>
 
 Missile::Missile(sf::Vector2f position, float angle) : Entity("resources/missile.png") {
+    type = Type::Missile;
+    // Drastically reducing hitbox for better collision with asteroids
+    hitbox.setScale(hitbox.getScale().x / 10, hitbox.getScale().y / 10);
     sprite.setPosition(position);
     this->angle = angle;
     sprite.setRotation(this->angle);
     speed = 3000.f;
-    type = Type::Missile;
 }
 
 void Missile::move(float delta_time) {
